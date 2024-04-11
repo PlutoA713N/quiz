d09f0c5c9dd305b2e6182924b5bc0aa12c9937ae
PlutoA713N <mallachandu99@gmail.com>
2024-04-11 10:02:20 +0530

    Add QuizResults.model.js

---
+++ models/QuizResults.model.js
@@ -0,0 +1,20 @@
+module.exports = mongoose => {
+    const { Schema, Types } = mongoose;
+  
+    const quizResultSchema = new Schema(
+      {
+         name: { type: String, required: true },
+         score: { type: Number }
+      },
+      { timestamps: true }
+  );
+  
+    quizResultSchema.method("toJSON", function () {
+        const { __v, _id, ...object } = this.toObject();
+        object.id = _id;
+        return object;
+    });
+  
+    const QuizResult = mongoose.model("QuizResult", quizResultSchema);
+    return QuizResult;
+};
